3c3792315acf5ba6459b2c3e120bcf6051dee6c8
N1GHT1130 <151446552+N1GHT1130@users.noreply.github.com>
2026-02-24 22:18:10 +0800

    webpage UI design ver1.0

--- coralguard/lib/main.dart
+++ coralguard/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'pages/home_page.dart';
 void main() {
-  runApp(const MainApp());
+  runApp(const CoralGuardApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CoralGuardApp extends StatelessWidget {
+  const CoralGuardApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'CoralGuard',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const HomePage(),
     );
   }
 }
